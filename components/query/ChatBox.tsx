8810b6b87acb85c7eb7accd8e63898d7dccac6f4
jjmacakiage <jj.macakiage@gmail.com>
2023-05-08 14:11:56 +0100

    query with collapses: tree | chat | console

--- components/query/ChatBox.tsx
+++ components/query/ChatBox.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import * as React from 'react';
-import { Box, Unstable_Grid2 as Grid, IconButton } from '@mui/material';
+import { Box, IconButton } from '@mui/material';
 import { SendSharp, CloseSharp } from '@mui/icons-material';
-import { borderRadius, padding, margin } from 'lib/constants';
+import { borderRadius, borderWidth, padding, margin } from 'lib/constants';
 import { DInput, DText } from 'components/common';
 
 interface ChatBoxProps {}
@@ -26,9 +26,12 @@ export const ChatBox: React.FC<ChatBoxProps> = props => {
         padding,
         backgroundColor: src == 'user' ? 'background.paper' : 'info.main',
         mb: margin,
-        maxWidth: 300,
-        ml: src == 'query' ? 0 : 'auto',
-        mr: src == 'user' ? 0 : 'auto',
+        width: 'auto',
+        maxWidth: 400,
+        mr: src == 'user' ? margin : 0,
+        alignSelf: src == 'user' ? 'flex-end' : 'flex-start',
+        position: 'relative',
+        float: src == 'user' ? 'right' : 'left',
       }}
       display="flex"
       flexDirection="row"
@@ -44,7 +47,7 @@ export const ChatBox: React.FC<ChatBoxProps> = props => {
   ) => setInputText(e.target.value);
 
   const sendQuery = (e: React.SyntheticEvent) => {
-    if (inputText.length == 0) {
+    if (inputText == '') {
       alert('Missing Input string');
       return;
     }
@@ -63,57 +66,53 @@ export const ChatBox: React.FC<ChatBoxProps> = props => {
   };
 
   return (
-    <Box
-      sx={{
-        backgroundColor: 'background.default',
-        borderRadius: borderRadius * 2,
-        padding: padding * 2,
-        flex: 1,
-        height: '100%'
-      }}
-    >
-      <Grid container direction="column">
-        <Grid xs={10}>
-          <Box sx={{ flex: 1, overflowY: 'auto', overflowX: 'hidden' }}>
-            {messages.map(({ text, src }, i) => messageBox(text, src, `${i}`))}
-          </Box>
-        </Grid>
-        <Grid
-          xs
-          display="flex"
-          flexDirection="row"
-          justifyContent="space-evenly"
-        >
-          <form
-            style={{
-              flex: 1,
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-            }}
-            onSubmit={e => {
-              e.preventDefault();
-              sendQuery(e);
-            }}
-          >
-            <DInput
-              fullWidth
-              variant="outlined"
-              onChange={handleInputChange}
-              sx={{ display: 'flex', alignItems: 'flex-end' }}
-              value={inputText}
-            >
-              <IconButton>
+    <>
+      <Box
+        display="flex"
+        flexDirection="column"
+        flexGrow={1}
+        flexShrink={1}
+        flexBasis="auto"
+        sx={{
+          overflowY: 'auto',
+          overflowX: 'hidden',
+          height: '80%',
+          mb: margin * 2,
+          borderWidth,
+          borderRadius,
+          padding: padding * 2,
+        }}
+      >
+        {messages.map(({ text, src }, i) => messageBox(text, src, `${i}`))}
+      </Box>
+      <Box
+        display="flex"
+        flexGrow={0}
+        flexShrink={1}
+        flexBasis="auto"
+        flexDirection="row"
+        justifyContent="space-evenly"
+        alignItems="stretch"
+      >
+        <DInput
+          fullWidth
+          variant="outlined"
+          onChange={handleInputChange}
+          onKeyDown={e => e.key == 'Enter' && sendQuery(e)}
+          sx={{ mr: margin }}
+          value={inputText}
+          InputProps={{
+            endAdornment: (
+              <IconButton onClick={e => setInputText('')}>
                 <CloseSharp sx={{ fontSize: 10 }} color="primary" />
               </IconButton>
-            </DInput>
-
-            <IconButton onClick={sendQuery} type="submit">
-              <SendSharp sx={{ fontSize: 16 }} color="primary" />
-            </IconButton>
-          </form>
-        </Grid>
-      </Grid>
-    </Box>
+            ),
+          }}
+        />
+        <IconButton onClick={sendQuery} type="submit">
+          <SendSharp sx={{ fontSize: 16 }} color="primary" />
+        </IconButton>
+      </Box>
+    </>
   );
 };
--- components/query/index.tsx
+++ components/query/index.tsx
@@ -2,43 +2,93 @@
 
 import * as React from 'react';
 import { m } from 'framer-motion';
-import { Box, useTheme, Unstable_Grid2 as Grid } from '@mui/material';
-import { padding, borderRadius } from 'lib/constants';
+import { Box, useTheme, Unstable_Grid2 as Grid, Collapse } from '@mui/material';
+import { padding, borderRadius, margin } from 'lib/constants';
 import { TreeView } from './TreeView';
 import { ChatBox } from './ChatBox';
+import { ExpandMoreButton } from 'components/common';
 
 interface ContainerProps {}
 
 export const Container: React.FC<ContainerProps> = props => {
   const theme = useTheme();
 
+  const [view, setView] = React.useState<{
+    [pane in 'tree' | 'chat' | 'console']: boolean;
+  }>({
+    tree: true,
+    chat: true,
+    console: true,
+  });
+
   return (
-    <Box onContextMenu={e => e.preventDefault()} sx={{ height: '100%' }}>
-      <Grid
-        container
-        columnSpacing={1}
-        display="flex"
-        justifyContent="space-between"
-        sx={{
-          paddingTop: padding * 2,
-          paddingBottom: padding * 2,
-          paddingLeft: padding * 2,
-          paddingRight: padding * 2,
-        }}
-      >
-        <Grid xs={3}>
+    <Box
+      onContextMenu={e => e.preventDefault()}
+      sx={{
+        position: 'absolute',
+        top: 150,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        paddingTop: padding * 2,
+        paddingBottom: padding * 2,
+        paddingLeft: padding * 2,
+        paddingRight: padding * 2,
+      }}
+      display="flex"
+      flexDirection="row"
+      alignItems="stretch"
+    >
+      <Box display="flex" flexGrow={1} flexShrink={1}>
+        <ExpandMoreButton
+        aria-label='Tree'
+          color="primary"
+          expanded={view.tree}
+          onClick={e => setView({ ...view, tree: view.tree ? false : true })}
+        />
+        <Collapse
+          in={view.tree}
+          easing="easeInOut"
+          orientation="horizontal"
+          component="div"
+          sx={{
+            display: 'flex',
+            flex: 1,
+            backgroundColor: 'info.main',
+            margin,
+          }}
+        >
           {/* <TreeView /> */}
-        </Grid>
-        <Grid xs={8} sx={{ height: '100%' }}>
-          <ChatBox />
-        </Grid>
-        <Grid xs>
-          <m.div
-            layout
-            style={{ backgroundColor: theme.palette.info.main }}
-          ></m.div>
-        </Grid>
-      </Grid>
+          TreeView Here
+        </Collapse>
+      </Box>
+      <Box flexGrow={1} flexShrink={1} flexBasis="100%" sx={{ margin }}>
+        <ChatBox />
+      </Box>
+      <Box display="flex" flexGrow={1} flexShrink={1}>
+        <Collapse
+          in={view.console}
+          easing="easeInOut"
+          orientation="horizontal"
+          component="div"
+          sx={{
+            display: 'flex',
+            flex: 1,
+            backgroundColor: 'info.main',
+            margin,
+          }}
+        >
+          Console Here
+        </Collapse>
+        <ExpandMoreButton
+          aria-label='Console'
+          color="primary"
+          expanded={!view.console}
+          onClick={e =>
+            setView({ ...view, console: view.console ? false : true })
+          }
+        />
+      </Box>
     </Box>
   );
 };
